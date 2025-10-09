85467000ee9e07247650c39ad7e1ee5155ccfb5d
galitz-matt <galitz.matthew@gmail.com>
2025-10-09 13:17:25 -0400

    Repository query functions

--- habitcircle/server/domain/circle/circle.repository.ts
+++ habitcircle/server/domain/circle/circle.repository.ts
@@ -2,6 +2,8 @@ import { Circle } from "./circle";
 
 export interface CircleRepository {
     findById(id: string): Promise<Circle | null>;
+    findByName(name: string): Promise<Circle[] | null>;
+    findCirclesByUserId(userId: string): Promise<Circle[] | null>;
     save(circle: Circle): Promise<void>;
     delete(id: string): Promise<void>;
 }
--- habitcircle/server/domain/completion/completion.repository.ts
+++ habitcircle/server/domain/completion/completion.repository.ts
@@ -1,6 +1,9 @@
 import { Completion } from "./completion";
 
 export interface CompletionRepository {
-    save(completion: Completion): Promise<void>
-    delete(id: string): Promise<void>
+    findByUserAndHabit(userId: string, habitId: string): Promise<Completion | null>
+    findByUserId(userId: string): Promise<Completion[] | null>;
+    findByHabitId(habitId: string): Promise<Completion[] | null>;
+    save(completion: Completion): Promise<void>;
+    delete(id: string): Promise<void>;
 }
