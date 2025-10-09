52752104e2d0922fbc2667ef38ac908b63e29601
galitz-matt <galitz.matthew@gmail.com>
2025-10-09 13:29:28 -0400

    Give Circle aggregate ownership over habits

--- habitcircle/server/domain/circle/circle.ts
+++ habitcircle/server/domain/circle/circle.ts
@@ -1,3 +1,4 @@
+import { Habit } from "../habit/habit";
 import { User } from "../user/user"
 
 export class Circle {
@@ -7,12 +8,25 @@ export class Circle {
         readonly name: string,
         readonly owner: User,
         readonly members: User[],
+        readonly habits: Habit[]
     ) {}
 
-    static create(name: string, owner: User, members: User[] = []): Circle {
+    static create(
+        name: string, 
+        owner: User, 
+        members: User[] = [], 
+        habits: Habit[] = []
+    ): Circle {
         if (!name.trim()) throw new Error("Name cannot be empty");
 
         const circleId = crypto.randomUUID();
-        return new Circle(circleId, new Date(), name, owner, members);
+        return new Circle(
+            circleId, 
+            new Date(), 
+            name, 
+            owner, 
+            members, 
+            habits
+        );
     }
 }
