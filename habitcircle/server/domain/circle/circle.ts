a1bff0c810b59133ad6d4b3eb2320116ace6236c
galitz-matt <galitz.matthew@gmail.com>
2025-10-09 14:04:57 -0400

    Create reusable Circle clone function

--- habitcircle/server/domain/circle/circle.ts
+++ habitcircle/server/domain/circle/circle.ts
@@ -30,52 +30,37 @@ export class Circle {
         );
     }
 
+    private clone(changes: Partial<Circle>): Circle {
+        return new Circle(
+            changes.id ?? this.id,
+            changes.createdAt ?? this.createdAt,
+            changes.name ?? this.name,
+            changes.owner ?? this.owner,
+            changes.members ?? this.members,
+            changes.habits ?? this.habits,
+        )
+    }
+
     addMember(user: User): Circle {
         if (this.members.some(u => u.id === user.id)) throw new Error("User is already member of this circle");
-        return new Circle(
-            this.id, 
-            this.createdAt, 
-            this.name, 
-            this.owner, 
-            [...this.members, user], 
-            this.habits
-        );
+        return this.clone({ members: [...this.members, user] });
     }
     
     addHabit(habit: Habit): Circle {
-        if (this.habits.some(h => h.id === habit.id)) throw new Error("Habit is already in this circle");
         if (habit.circleId !== this.id) throw new Error("Habit does not belong to this circle")
-        return new Circle(
-            this.id, 
-            this.createdAt, 
-            this.name, 
-            this.owner, 
-            this.members, 
-            [...this.habits, habit]
-        );
+        if (this.habits.some(h => h.id === habit.id)) throw new Error("Habit is already in this circle");
+        return this.clone({ habits: [...this.habits, habit] })
     }
 
     deleteMember(user: User): Circle {
+        if (user.id === this.owner.id) throw new Error("Owner cannot be removed from their own circle");
         if (!this.members.find(u => u.id === user.id)) throw new Error("User is not in circle");
-        return new Circle(
-            this.id, 
-            this.createdAt, 
-            this.name, 
-            this.owner, 
-            this.members.filter(u => u.id !== user.id), 
-            this.habits
-        );
+        return this.clone({ members: this.members.filter(u => u.id !== user.id) })
     }
 
     deleteHabit(habit: Habit): Circle {
+        if (habit.circleId !== this.id) throw new Error("Habit does not belong to this circle");
         if (!this.habits.find(h => h.id === habit.id)) throw new Error("Habit is not in circle");
-        return new Circle(
-            this.id, 
-            this.createdAt, 
-            this.name, 
-            this.owner, 
-            this.members,
-            this.habits.filter(h => h.id !== habit.id)
-        );
+        return this.clone({ habits: this.habits.filter(h => h.id !== habit.id )})
     }
 }
