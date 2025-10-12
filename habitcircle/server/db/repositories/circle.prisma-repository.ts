a2ffa0dec55643d31c60d30804243c7f3c0e7733
galitz-matt <galitz.matthew@gmail.com>
2025-10-12 18:25:09 -0400

    Implement circle repo for prisma ORM

---
+++ habitcircle/server/db/repositories/circle.prisma-repository.ts
@@ -0,0 +1,65 @@
+import { Circle } from "@server/domain/circle/circle.entity";
+import { CircleRepository } from "@server/domain/circle/circle.repository";
+import { CircleMapper } from "../mappers/circle.mapper";
+import { PrismaClient } from "@generated/prisma";
+
+export class CirclePrismaRepository implements CircleRepository {
+    constructor(private readonly prisma: PrismaClient) {}
+
+    async findById(id: string): Promise<Circle | null> {
+        const circleRecord = await this.prisma.circle.findUnique({
+            where: { id : id },
+            include: { owner: true, members: true, habits: true }
+        });
+
+        return circleRecord ? CircleMapper.toDomain(circleRecord) : null;
+    }
+
+    async findByName(name: string): Promise<Circle[] | null> {
+        const circleRecords = await this.prisma.circle.findMany({
+            where: { name: name },
+            include: { owner: true, members: true, habits: true }
+        });
+
+        return circleRecords ? circleRecords.map(cr => CircleMapper.toDomain(cr)) : null;
+    }
+
+    async findCirclesByUserId(userId: string): Promise<Circle[] | null> {
+        const userRecord = await this.prisma.user.findUnique({
+            where: { id: userId },
+            include: { 
+                circles: {
+                    include: { owner: true, members: true, habits: true}
+                } 
+            }
+        });
+
+        return userRecord ? userRecord.circles.map(cr => CircleMapper.toDomain(cr)) : null;
+    }
+
+    async save(circle: Circle): Promise<void> {
+        const base = CircleMapper.toPrisma(circle);
+
+        await this.prisma.circle.upsert({
+            where: { id: circle.id },
+            create: {
+                ...base,
+                owner: { connect: { id: circle.owner.id } },
+                members: { connect: circle.members.map(m => ({ id: m.id })) },
+                habits: { connect: circle.habits.map(h => ({ id: h.id })) }
+            },
+            update: {
+                ...base,
+                owner: { connect: { id: circle.owner.id }},
+                members: { set: circle.members.map(m => ({ id: m.id })) },
+                habits: { set: circle.habits.map(h => ({ id: h.id })) }
+            }
+        });
+    }
+
+    async delete(id: string): Promise<void> {
+        await this.prisma.circle.delete({
+            where: { id: id }
+        });
+    }
+}
