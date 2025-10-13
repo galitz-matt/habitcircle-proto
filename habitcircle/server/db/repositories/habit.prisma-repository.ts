9aa7b486085199298a8ece10d0f9fb51cd8dcd84
galitz-matt <galitz.matthew@gmail.com>
2025-10-13 12:05:33 -0400

    Implement Habit repository for Prisma ORM

---
+++ habitcircle/server/db/repositories/habit.prisma-repository.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from "@generated/prisma";
+import { HabitRepository } from "@server/domain/habit/habit.repository";
+import { HabitMapper } from "../mappers/habit.mapper";
+import { Habit } from "@server/domain/habit/habit.entity";
+
+export class HabitPrismaRepository implements HabitRepository {
+    constructor(private readonly prisma: PrismaClient) {}
+
+    async findById(id: string): Promise<Habit | null> {
+        const habitRecord = await this.prisma.habit.findUnique({
+            where: { id: id },
+        })
+
+        return habitRecord ? HabitMapper.toDomain(habitRecord) : null;
+    }
+
+    async findByCircleId(circleId: string): Promise<Habit[] | null> {
+        const circleRecord = await this.prisma.circle.findUnique({
+            where: { id: circleId },
+            include: { habits: true }
+        });
+
+        return circleRecord ? circleRecord.habits.map(h => HabitMapper.toDomain(h)) : null;
+    }
+
+    async save(habit: Habit): Promise<void> {
+        const habitRecord = HabitMapper.toPrisma(habit);
+
+        await this.prisma.habit.upsert({
+            where: { id: habitRecord.id },
+            update: habitRecord,
+            create: habitRecord
+        });
+    }
+
+    async delete(id: string): Promise<void> {
+        await this.prisma.habit.delete({
+            where: { id: id }
+        });
+    }
+}
