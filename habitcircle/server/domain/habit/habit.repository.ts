6e11b56a34505e51c6b9b0bb3298e73c51e8729a
galitz-matt <galitz.matthew@gmail.com>
2025-10-13 12:05:09 -0400

    Modify return type to include null results

--- habitcircle/server/domain/habit/habit.repository.ts
+++ habitcircle/server/domain/habit/habit.repository.ts
@@ -2,7 +2,7 @@ import { Habit } from "./habit.entity";
 
 export interface HabitRepository {
     findById(id: string): Promise<Habit | null>;
-    findByCircleId(circleId: string): Promise<Habit[]>;
+    findByCircleId(circleId: string): Promise<Habit[] | null>;
     save(habit: Habit): Promise<void>;
     delete(id: string): Promise<void>;
 }
