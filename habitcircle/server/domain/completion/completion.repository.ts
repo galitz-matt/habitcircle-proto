b4b4bbf4a738e7b6c67f19dc276e2e6b83cf4930
galitz-matt <galitz.matthew@gmail.com>
2025-10-13 22:11:51 -0400

    Add findAll methods to repo interfaces

--- habitcircle/server/domain/completion/completion.repository.ts
+++ habitcircle/server/domain/completion/completion.repository.ts
@@ -2,8 +2,9 @@ import { Completion } from "./completion.entity";
 
 export interface CompletionRepository {
     findByUserAndHabit(userId: string, habitId: string): Promise<Completion | null>
-    findByUserId(userId: string): Promise<Completion[] | null>;
-    findByHabitId(habitId: string): Promise<Completion[] | null>;
+    findByUserId(userId: string): Promise<Completion[]>;
+    findByHabitId(habitId: string): Promise<Completion[]>;
+    findAll(): Promise<Completion[]>;
     save(completion: Completion): Promise<void>;
     delete(id: string): Promise<void>;
 }
--- habitcircle/server/domain/user/user.repository.ts
+++ habitcircle/server/domain/user/user.repository.ts
@@ -3,6 +3,7 @@ import { User } from "./user.entity";
 export interface UserRepository {
     findById(id: string): Promise<User | null>;
     findByUsername(username: string): Promise<User | null>;
+    findAll(): Promise<User[]>;
     save(user: User): Promise<void>;
     delete(id: string): Promise<void>;
 }
