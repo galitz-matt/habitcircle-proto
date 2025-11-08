6e291ad16816e78d725139ab039bd6853b63a03d
galitz-matt <galitz.matthew@gmail.com>
2025-11-07 22:39:14 -0500

    Styling, dependency renames

--- habitcircle/server/application/services/user.service.ts
+++ habitcircle/server/application/services/user.service.ts
@@ -3,32 +3,32 @@ import { User } from "@/server/domain/entities/user.entity";
 import { RegisterUserCommand, RegisterUserResult } from "../dto/user/register-user.dto";
 import { DeleteUserCommand, DeleteUserResult } from "../dto/user/delete-user.dto";
 import { Result } from "@/lib/types";
-import { badResult } from "@/lib/utils";
+import { serviceFailure } from "@/lib/utils";
 
 export class UserService {
     constructor(
         private readonly userRepo: UserRepository
     ) {}
 
-    async register(cmd: RegisterUserCommand): Promise<Result<RegisterUserResult>> {
+    async registerUser(cmd: RegisterUserCommand): Promise<Result<RegisterUserResult>> {
         try {
             const user = await User.create(cmd.username, cmd.password);
             await this.userRepo.save(user)
             return { ok: true, value: { userId: user.id, username: user.getUsername() }}
         } catch (err) {
-            return badResult(err);
+            return serviceFailure(err);
         }
     }
 
-    async delete(cmd: DeleteUserCommand): Promise<Result<DeleteUserResult>> {
+    async deleteUser(cmd: DeleteUserCommand): Promise<Result<DeleteUserResult>> {
         if (cmd.requestingUserId !== cmd.targetUserId)
-            return badResult("Cannot delete another user");
+            return serviceFailure("Cannot delete another user");
         
         try {
             await this.userRepo.delete(cmd.targetUserId);
             return { ok: true, value: { result: true }}
         } catch (err) {
-            return badResult(err);
+            return serviceFailure(err);
         }
     }
 }
