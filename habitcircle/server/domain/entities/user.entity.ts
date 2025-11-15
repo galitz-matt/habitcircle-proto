acd8cb26c00aef183a1b480d81a32e6ae9a382cb
galitz-matt <galitz.matthew@gmail.com>
2025-11-15 15:02:18 -0500

    Extract auth details from User entity

--- habitcircle/server/domain/entities/user.entity.ts
+++ habitcircle/server/domain/entities/user.entity.ts
@@ -3,16 +3,13 @@ import { Username } from "@/server/domain/value-objects/username.value-object";
 import { Password } from "@/server/domain/value-objects/password.value-object";
 import { EmailAddress } from "@/server/domain/value-objects/email-address.value-object";
 import { Biography } from "@/server/domain/value-objects/biography.value-object";
-import { DomainError } from "@/lib/errors";
 
 export class User {
     private constructor(
         readonly id: string,
         readonly createdAt: Date,
-        readonly updatedAt: Date,
         readonly username: Username,
         readonly emailAddress?: EmailAddress,
-        readonly password?: Password,
         readonly biography?: Biography, 
         readonly profilePictureKey?: string 
     ) {}
@@ -21,10 +18,8 @@ export class User {
         return new User(
             IdGenerator.new(),
             new Date(),
-            new Date(),
             username,
             emailAddress,
-            password
         );
     };
 
@@ -44,10 +39,6 @@ export class User {
         return this.username.toString();
     }
 
-    getPasswordHash(): string | undefined {
-        return !!this.password ? this.password.toString() : undefined;
-    }
-
     getProfilePictureKey(): string | undefined {
         return !!this.profilePictureKey ? this.profilePictureKey.toString() : undefined;
     }
@@ -55,10 +46,8 @@ export class User {
     static rehydrate(
         id: string,
         createdAt: Date,
-        updatedAt: Date,
         username: string,
         emailAddress?: string,
-        password?: string,
         biography?: string,
         profilePictureKey?: string
     ): User {
@@ -66,31 +55,19 @@ export class User {
         return new User(
             id, 
             createdAt, 
-            updatedAt,
             Username.rehydrate(username), 
             emailAddress ? EmailAddress.rehydrate(emailAddress) : undefined,
-            password ? Password.rehydrate(password) : undefined,
             biography ? Biography.rehydrate(biography) : undefined,
             profilePictureKey ? profilePictureKey : undefined,
         );
-    }
-
-    async verifyPassword(plain: string): Promise<boolean> {
-        if (!!this.password === false) {
-            throw new DomainError("User does not have a password");
-        }
-
-        return !!this.password.matches(plain);
     }
 
     private clone(changes: Partial<User>): User {
         return new User(
             changes.id ?? this.id,
             changes.createdAt ?? this.createdAt,
-            changes.updatedAt ?? this.updatedAt,
             changes.username ?? this.username,
             changes.emailAddress ?? this.emailAddress,
-            changes.password ?? this.password,
             changes.biography ?? this.biography,
             changes.profilePictureKey ?? this.profilePictureKey
         )
