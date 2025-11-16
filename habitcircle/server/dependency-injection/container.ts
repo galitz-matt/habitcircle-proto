e0bc03bf7b29caef2a5f9cac0562f52fe34d2e9d
galitz-matt <galitz.matthew@gmail.com>
2025-11-15 22:40:09 -0500

    Fix imports

--- habitcircle/server/dependency-injection/container.ts
+++ habitcircle/server/dependency-injection/container.ts
@@ -1,22 +1,13 @@
 import { container } from "tsyringe";
-import { CircleRepository } from "@/server/domain/repositories/circle.repository";
-import { UserRepository } from "@/server/domain/repositories/user.repository";
-import { HabitRepository } from "@/server/domain/repositories/habit.repository";
-import { CirclePrismaRepository } from "@/server/infrastructure/db/repositories/circle.prisma-repository";
-import { UserPrismaRepository } from "@/server/infrastructure/db/repositories/user.prisma-repository";
-import { HabitPrismaRepository } from "@/server/infrastructure/db/repositories/habit.prisma-repository";
-import { CircleService } from "@/server/application/services/circle.service";
-import { HabitService } from "./application/services/habit.service";
-import { UserService } from "./application/services/user.service";
-import { AuthenticationService } from "./application/services/authentication.service";
+import { CircleRepository } from "@/server/application/repositories/circle.repository";
+import { UserRepository } from "@/server/application/repositories/user.repository";
+import { HabitRepository } from "@/server/application/repositories/habit.repository";
+import { CirclePrismaRepository } from "@/server/infrastructure/db/prisma/repositories/circle.prisma-repository";
+import { UserPrismaRepository } from "@/server/infrastructure/db/prisma/repositories/user.prisma-repository";
+import { HabitPrismaRepository } from "@/server/infrastructure/db/prisma/repositories/habit.prisma-repository";
 
 container.register<CircleRepository>("CircleRepository", { useClass: CirclePrismaRepository });
 container.register<UserRepository>("UserRepository", { useClass: UserPrismaRepository });
 container.register<HabitRepository>("HabitRepository", { useClass: HabitPrismaRepository });
-
-container.register("AuthenticationService", { useClass: AuthenticationService })
-container.register("CircleService", { useClass: CircleService });
-container.register("HabitService", { useClass: HabitService })
-container.register("UserService", { useClass: UserService })
 
 export { container };
