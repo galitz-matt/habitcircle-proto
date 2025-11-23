a0e86f40814b5652c8293a6313f372c2ca729aab
galitz-matt <galitz.matthew@gmail.com>
2025-11-23 14:04:40 -0500

    Rename fields/types

--- habitcircle/server/domain/value-objects/auth/authentication.interface.ts
+++ habitcircle/server/domain/value-objects/auth/authentication.interface.ts
@@ -1,7 +1,7 @@
 import { DomainAuthType } from "@/server/domain/types/auth-type";
-import { AuthDto } from "../../dtos/auth/authentication.dto";
+import { AuthenticationDto } from "../../dtos/auth/authentication.dto";
 
 export interface Authentication {
     readonly type: DomainAuthType
-    getAuthInfo(): AuthDto
+    getAuthInfo(): AuthenticationDto
 }
