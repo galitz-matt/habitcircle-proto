76833157a6f09946a2b5664dd6139e9308c16f7d
galitz-matt <galitz.matthew@gmail.com>
2025-11-24 20:20:20 -0500

    Remove props

--- habitcircle/server/domain/entities/post.entity.ts
+++ habitcircle/server/domain/entities/post.entity.ts
@@ -1,70 +1,52 @@
 import { IdGenerator } from "@/lib/utils";
 import { PostCaption } from "../value-objects/post-caption.value-object";
 
-export type PostProps = {
-    id: string,
-    createdAt: Date,
-    userId: string,
-    habitId: string,
-    completionId: string,
-    photoKey?: string,
-    caption?: PostCaption
-}
-
-export type CreatePostInput = {
-    userId: string,
-    habitId: string,
-    completionId: string,
-    photoKey?: string,
-    caption?: PostCaption
-}
-
 export class Post {
-    private constructor(readonly props: PostProps) {}
+    private constructor(
+        private readonly _id: string,
+        private readonly _createdAt: Date,
+        private readonly _userId: string,
+        private readonly _habitId: string,
+        private readonly _completionId: string,
+        private _photoKey?: string,
+        private _caption?: PostCaption
+    ) {}
 
-    static create(input: CreatePostInput): Post {
-        const props: PostProps = {
-            id: IdGenerator.new(),
-            createdAt: new Date(),
-            ...input
-        };
-
-        return new Post(props)
+    static create(
+        userId: string,
+        habitId: string,
+        completionId: string,
+        photoKey?: string,
+        caption?: PostCaption
+    ): Post {
+        return new Post(
+            IdGenerator.new(),
+            new Date(),
+            userId,
+            habitId,
+            completionId,
+            photoKey,
+            caption
+        )
     }
 
-    static rehydrate(props: PostProps): Post {
-        return new Post(props);
-    }
-
-    get id(): string {
-        return this.props.id;
-    }
-
-    get createdAt(): Date {
-        return this.props.createdAt;
-    }
-
-    get userId(): string {
-        return this.props.userId;
-    }
-
-    get habitId(): string {
-        return this.props.habitId;
-    }
-
-    get completionId(): string {
-        return this.props.completionId;
-    }
-    
-    get photoKey(): string | undefined {
-        return this.props.photoKey;
-    }
-
-    get caption(): PostCaption | undefined {
-        return this.props.caption;
-    }
-
-    protected create(props: PostProps): this {
-        return new Post(props) as this;
+    static rehydrate(
+        id: string,
+        createdAt: Date,
+        userId: string,
+        habitId: string,
+        completionId: string,
+        photoKey?: string,
+        caption?: PostCaption
+    ): Post {
+        return new Post(
+            id,
+            createdAt,
+            userId,
+            habitId,
+            completionId,
+            photoKey,
+            caption
+        );
     }
 }
