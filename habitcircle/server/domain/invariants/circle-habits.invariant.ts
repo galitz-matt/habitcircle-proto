20a49d739fb2643bc3909f9d343bdcde942417d2
galitz-matt <galitz.matthew@gmail.com>
2025-11-25 22:28:02 -0500

    feat: guard against duplicate ids and cross circle habits

--- habitcircle/server/domain/invariants/circle-habits.invariant.ts
+++ habitcircle/server/domain/invariants/circle-habits.invariant.ts
@@ -2,14 +2,26 @@ import { Habit } from "@/server/domain/entities/habit.entity";
 import { DomainError } from "@/lib/errors";
 
 export class CircleHabitsInvariants {
-    static enforce(habits: Habit[]): void {
-        this.ensureNoDuplicates(habits);
+    static enforce(habits: Set<Habit>): void {
+        this.ensureNoDuplicateIds(habits);
+        this.ensureNoCrossCircleHabits(habits);
     }
 
-    static ensureNoDuplicates(habits: Habit[]): void {
-        const habitSet = new Set(habits);
-        if (habits.length !== habitSet.size) {
-            throw new DomainError("Found duplicate habits.")
-        }
+    static ensureNoDuplicateIds(habits: Set<Habit>): void {
+        const ids = new Set<string>();
+        for (const habit of habits)
+            ids.add(habit.id);
+
+        if (ids.size !== habits.size)
+            throw new DomainError("CircleHabits cannot contain duplicate ids");
     }
+
+    static ensureNoCrossCircleHabits(habits: Set<Habit>): void {
+        const ids = new Set<string>();
+        for (const habit of habits)
+            ids.add(habit.id);
+
+        if (ids.size !== 1)
+            throw new DomainError("CircleHabits cannot contain cross-circle habits");
+    }   
 }
