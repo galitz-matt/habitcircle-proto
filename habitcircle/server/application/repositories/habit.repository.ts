3744a5a55b35c5e2247c3bd52854640a8388b5cd
galitz-matt <galitz.matthew@gmail.com>
2025-11-30 13:52:58 -0500

    feat: create Habit repo interface

---
+++ habitcircle/server/application/repositories/habit.repository.ts
@@ -0,0 +1,8 @@
+import { Habit } from "@/server/domain/entities/habit.entity";
+
+export interface HabitRepository {
+    findById(id: string): Promise<Habit | null>;
+    findByCircleId(circleId: string): Promise<Habit[]>;
+    save(habit: Habit): Promise<void>;
+    delete(id: string): Promise<void>;
+}
