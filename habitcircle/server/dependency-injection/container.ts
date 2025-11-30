5a7aa067b786b862237bd5e39dac86146121d3e6
galitz-matt <galitz.matthew@gmail.com>
2025-11-29 20:11:24 -0500

    fix: forgot

--- habitcircle/server/dependency-injection/container.ts
+++ habitcircle/server/dependency-injection/container.ts
@@ -1,13 +1,13 @@
 import { container } from "tsyringe";
 import { CircleRepository } from "@/server/application/repositories/circle.repository";
 import { UserRepository } from "@/server/application/repositories/user.repository";
-import { HabitRepository } from "@/server/application/repositories/habit.repository";
-import { CirclePrismaRepository } from "@/server/infrastructure/db/prisma/repositories/circle.prisma-repository";
-import { UserPrismaRepository } from "@/server/infrastructure/db/prisma/repositories/user.prisma-repository";
-import { HabitPrismaRepository } from "@/server/infrastructure/db/prisma/repositories/habit.prisma-repository";
+// import { HabitRepository } from "@/server/application/repositories/habit.repository";
+import { CirclePrismaRepository } from "@/server/infra/db/prisma/repositories/circle.prisma-repository";
+import { UserPrismaRepository } from "@/server/infra/db/prisma/repositories/user.prisma-repository";
+import { HabitPrismaRepository } from "@/server/infra/db/prisma/repositories/habit.prisma-repository";
 
 container.register<CircleRepository>("CircleRepository", { useClass: CirclePrismaRepository });
 container.register<UserRepository>("UserRepository", { useClass: UserPrismaRepository });
-container.register<HabitRepository>("HabitRepository", { useClass: HabitPrismaRepository });
+// container.register<HabitRepository>("HabitRepository", { useClass: HabitPrismaRepository });
 
 export { container };
