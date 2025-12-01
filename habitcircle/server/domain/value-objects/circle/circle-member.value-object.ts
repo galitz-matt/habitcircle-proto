d2f91c098037458bbed637cc7fbe5a1c21869356
galitz-matt <galitz.matthew@gmail.com>
2025-11-30 23:28:12 -0500

    feat: introduce CircleMember VO to encapsulate subset of User info

---
+++ habitcircle/server/domain/value-objects/circle/circle-member.value-object.ts
@@ -0,0 +1,40 @@
+import { Username } from "@/server/domain/value-objects/username.value-object";
+import { User } from "@/server/domain/entities/user.entity";
+
+export class CircleMember {
+    private constructor(
+        readonly userId: string,
+        readonly username: Username,
+        readonly profilePictureKey?: string,
+    ) {
+        Object.freeze(this);
+    }
+
+    static create(
+        userId: string,
+        username: Username,
+        profilePictureKey?: string
+    ) {
+        return new CircleMember(userId, username, profilePictureKey);
+    }
+
+    static fromUser(user: User) {
+        return CircleMember.create(
+            user.id,
+            user.username,
+            user.profilePictureKey
+        );
+    }
+
+    static rehydrate(
+        userId: string,
+        username: Username,
+        profilePictureKey?: string,
+    ): CircleMember {
+        return new CircleMember(
+            userId,
+            username,
+            profilePictureKey
+        )
+    }
+}
