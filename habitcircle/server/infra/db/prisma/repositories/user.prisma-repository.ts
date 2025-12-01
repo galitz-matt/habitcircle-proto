10b06bac0bd2093f8cfe17027e55d7058945f18c
galitz-matt <galitz.matthew@gmail.com>
2025-11-30 22:05:05 -0500

    fix: handle persistence of accounts in User repo, user is root aggregate

--- habitcircle/server/infra/db/prisma/repositories/user.prisma-repository.ts
+++ habitcircle/server/infra/db/prisma/repositories/user.prisma-repository.ts
@@ -3,44 +3,88 @@ import { UserRepository } from "@/server/application/repositories/user.repositor
 import { UserPrismaMapper } from "@/server/infra/db/prisma/mappers/user.prisma-mapper";
 import type { User } from "@/server/domain/entities/user.entity";
 import type { Username } from "@/server/domain/value-objects/username.value-object";
-import { NotFoundError } from "@/lib/errors";
+import { DuplicateError, NotFoundError } from "@/lib/errors";
 
 export class UserPrismaRepository implements UserRepository {
     constructor(private readonly prisma: PrismaClient) {}
 
     async findById(id: string): Promise<User | null> {
         const userRecord = await this.prisma.user.findUnique({
-            where: { id: id }
+            where: { id },
+            include: { oauthAccounts: true, credentialsAccount: true }
         });
         return userRecord ? UserPrismaMapper.toDomain(userRecord) : null; 
     }
 
     async findByUsername(username: Username): Promise<User | null> {
         const userRecord = await this.prisma.user.findUnique({
-            where: { username: username.toString() }
+            where: { username: username.toString() },
+            include: { oauthAccounts: true, credentialsAccount: true }
         });
         return userRecord ? UserPrismaMapper.toDomain(userRecord) : null;
     }
 
     async create(user: User): Promise<void> {
-        const userDto = UserPrismaMapper.toPersistence(user);
+        const { user: u, credentialsAccount, oauthAccounts } = UserPrismaMapper.toPersistence(user);
 
         await this.prisma.user.create({
-            data: userDto
+            data: {
+                ...u,
+                credentialsAccount: credentialsAccount
+                    ? { create: credentialsAccount }
+                    : undefined,
+                oauthAccounts: {
+                    create: oauthAccounts
+                }
+            }
+        }).catch((err) => {
+            if (err.code === "P2002") {
+                const target = err.meta?.target?.[0] ?? "unknown field";
+                throw new DuplicateError(`User already exists with duplicate ${target}`);
+            }
+            throw err;
         });
     }
 
     async update(user: User): Promise<void> {
-        const userDto = UserPrismaMapper.toPersistence(user);
+        const { user: u, credentialsAccount, oauthAccounts } = UserPrismaMapper.toPersistence(user);
+
+        const data = {
+            username: u.username,
+            emailAddress: u.emailAddress,
+            biography: u.biography,
+            profilePictureKey: u.profilePictureKey,
+            updatedAt: new Date(),
+
+            credentialsAccount: {},
+            oauthAccounts: {},
+        };
+
+        if (credentialsAccount) {
+            data.credentialsAccount = {
+                upsert: {
+                    create: credentialsAccount,
+                    update: credentialsAccount
+                },
+            };
+        } else {
+            data.credentialsAccount = {
+                delete: true,
+            };
+        }
+
+        data.oauthAccounts = {
+            deleteMany: {},
+            create: oauthAccounts,
+        };
 
         await this.prisma.user.update({
-            where: {
-                id: userDto.id,
-            },
-            data: {
-                ...userDto,
-                updatedAt: new Date(),
-            }
+            where: { id: u.id },
+            data,
+        }).catch((err) => {
+            if (err.code === "P2025")
+                throw new NotFoundError(`User with id ${u.id} not found`);
+            throw err;
         });
     }
 
@@ -48,7 +92,8 @@ export class UserPrismaRepository implements UserRepository {
         await this.prisma.user.delete({
             where: { id: user.id }
         }).catch((err) => {
-            if (err.code === "P2025") throw new NotFoundError(`User with id ${user.id} not found`);
+            if (err.code === "P2025") 
+                throw new NotFoundError(`User with id ${user.id} not found`);
             throw err;
         });
     }
