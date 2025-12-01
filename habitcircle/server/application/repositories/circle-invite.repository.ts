c6ba8c13449d246998d28d922dca69d381228056
galitz-matt <galitz.matthew@gmail.com>
2025-11-30 19:24:40 -0500

    style: rename params

--- habitcircle/server/application/repositories/circle-invite.repository.ts
+++ habitcircle/server/application/repositories/circle-invite.repository.ts
@@ -2,8 +2,8 @@ import { CircleInvite } from "@/server/domain/entities/circle-invite.entity";
 
 export interface CircleInviteRepository {
     findById(id: string): Promise<CircleInvite | null>;
-    findByRecipientId(userId: string): Promise<CircleInvite[]>;
-    findBySenderId(userId: string): Promise<CircleInvite[]>;
+    findByRecipientId(recipientId: string): Promise<CircleInvite[]>;
+    findBySenderId(senderId: string): Promise<CircleInvite[]>;
     create(circleInvite: CircleInvite): Promise<void>;
     update(circleInvite: CircleInvite): Promise<void>;
     delete(circleInvite: CircleInvite): Promise<void>;
