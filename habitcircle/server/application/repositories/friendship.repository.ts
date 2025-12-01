59650e1eb2f6c1b0b79e3acf886b34d07a930ee5
galitz-matt <galitz.matthew@gmail.com>
2025-11-30 19:13:56 -0500

    refactor: rename parameters

--- habitcircle/server/application/repositories/friendship.repository.ts
+++ habitcircle/server/application/repositories/friendship.repository.ts
@@ -2,8 +2,8 @@ import { Friendship } from "@/server/domain/entities/friendship.entity";
 
 export interface FriendshipRepository {
     findById(id: string): Promise<Friendship | null>;
-    findByRequesterId(userId: string): Promise<Friendship[]>;
-    findByAddresseeId(userId: string): Promise<Friendship[]>;
+    findByRequesterId(requesterId: string): Promise<Friendship[]>;
+    findByAddresseeId(addresseeId: string): Promise<Friendship[]>;
     create(friendship: Friendship): Promise<void>;
     update(friendship: Friendship): Promise<void>;
     delete(friendship: Friendship): Promise<void>
