c287fa490edfee5649adad30280a67b77b23df74
galitz-matt <galitz.matthew@gmail.com>
2025-11-30 21:35:21 -0500

    fix: include account props

--- habitcircle/server/infra/db/prisma/dtos/user-prisma.dto.ts
+++ habitcircle/server/infra/db/prisma/dtos/user-prisma.dto.ts
@@ -1,8 +1,13 @@
+import type { CredentialsAccountPrismaDto } from "./credentials-account-prisma.dto"
+import type { OAuthAccountPrismaDto } from "./oauth-account-prisma.dto";
+
 export type UserPrismaDto = {
-    id: string,
-    createdAt: Date,
-    username: string,
-    emailAddress: string | null,
-    biography: string | null,
-    profilePictureKey: string | null
+    id: string;
+    createdAt: Date;
+    username: string;
+    emailAddress: string | null;
+    biography: string | null;
+    profilePictureKey: string | null;
+    credentialsAccount: CredentialsAccountPrismaDto | null;
+    oauthAccounts: OAuthAccountPrismaDto[];
 }
