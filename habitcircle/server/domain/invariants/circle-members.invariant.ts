4e9a1b311433a54658efb8dea04fb7529a79cf02
galitz-matt <galitz.matthew@gmail.com>
2025-11-30 23:29:38 -0500

    refactor: accept CircleMembers

--- habitcircle/server/domain/invariants/circle-members.invariant.ts
+++ habitcircle/server/domain/invariants/circle-members.invariant.ts
@@ -1,14 +1,14 @@
-import { User } from "@/server/domain/entities/user.entity";
 import { DomainError } from "@/lib/errors";
+import { CircleMember } from "@/server/domain/value-objects/circle/circle-member.value-object";
 
 export class CircleMembersInvariants {
     
-    static enforce(members: User[]): void {
+    static enforce(members: CircleMember[]): void {
         this.ensureNoDuplicates(members);
     }
 
-    static ensureNoDuplicates(members: User[]): void  {
-        const membersSet = new Set(members.map(u => u.id));
+    static ensureNoDuplicates(members: CircleMember[]): void  {
+        const membersSet = new Set(members.map(u => u.userId));
         if (membersSet.size !== members.length) {
             throw new DomainError("Cannot contain duplicate members");
         }
