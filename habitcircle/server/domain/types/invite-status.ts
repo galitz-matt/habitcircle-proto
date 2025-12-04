20665744208e8e3e783079d7f0619fe059614336
galitz-matt <galitz.matthew@gmail.com>
2025-12-03 22:31:26 -0500

    refactor: convert enum to value-to-type

--- habitcircle/server/domain/types/invite-status.ts
+++ habitcircle/server/domain/types/invite-status.ts
@@ -1,5 +1,7 @@
-export enum DomainInviteStatus {
-    PENDING,
-    ACCEPTED,
-    DECLINED
+export const DomainInviteStatus = {
+    PENDING: "PENDING",
+    ACCEPTED: "ACCEPTED",
+    DECLINED: "DECLINED"
 }
+
+export type DomainInviteStatus = (typeof DomainInviteStatus)[keyof typeof DomainInviteStatus]
