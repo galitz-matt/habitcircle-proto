ab417f000fd84d7cd8aa32e5b22173ddc38f81d7
galitz-matt <galitz.matthew@gmail.com>
2025-12-07 20:13:59 -0500

    refactor: rename prop

--- habitcircle/server/infra/db/prisma/dtos/circle-prisma.dto.ts
+++ habitcircle/server/infra/db/prisma/dtos/circle-prisma.dto.ts
@@ -11,7 +11,7 @@ type CircleBasePrismaDto = {
 }
 
 export type CircleCreatePrismaDto = CircleBasePrismaDto & {
-    habits: HabitPrismaDto[];
+    habitsToCreate: HabitPrismaDto[];
 };
 
 export type CircleUpdatePrismaDto = CircleBasePrismaDto & {
