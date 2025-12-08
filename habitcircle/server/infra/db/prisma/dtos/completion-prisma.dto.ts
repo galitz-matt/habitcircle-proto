e719651435c419b0addb6b52756b9e5170289831
galitz-matt <galitz.matthew@gmail.com>
2025-12-07 21:31:13 -0500

    refactor: extract mutable Post props to separate DTO

--- habitcircle/server/infra/db/prisma/dtos/completion-prisma.dto.ts
+++ habitcircle/server/infra/db/prisma/dtos/completion-prisma.dto.ts
@@ -1,6 +1,20 @@
-export type CompletionPrismaDto = {
+import type { PostMutablePropsPrismaDto, PostPrismaDto } from "./post-prisma.dto"
+
+export type CompletionBasePrismaDto = {
     id: string,
     completedAt: Date,
     userId: string,
     habitId: string
+}
+
+export type CompletionCreatePrismaDto = CompletionBasePrismaDto & {
+    postToCreate: PostPrismaDto | undefined;
+}
+
+export type CompletionUpdatePrismaDto = CompletionBasePrismaDto & {
+    postToUpsert: {
+        where: { id: string };
+        create: PostPrismaDto;
+        update: PostMutablePropsPrismaDto;
+    } | undefined
 }
--- habitcircle/server/infra/db/prisma/dtos/post-prisma.dto.ts
+++ habitcircle/server/infra/db/prisma/dtos/post-prisma.dto.ts
@@ -1,9 +1,12 @@
+export type PostMutablePropsPrismaDto = {
+    photoKey: string | undefined;
+    caption: string | undefined; 
+}
+
 export type PostPrismaDto = {
     id: string;
     createdAt: Date;
     userId: string;
     habitId: string;
     completionId: string;
-    photoKey: string | undefined;
-    caption: string | undefined;
-}
+} & PostMutablePropsPrismaDto
