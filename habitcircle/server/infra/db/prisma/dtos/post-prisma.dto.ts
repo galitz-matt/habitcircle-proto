ece003393f4dc8334c9d67e598f7c782247458fb
galitz-matt <galitz.matthew@gmail.com>
2025-12-08 21:55:55 -0500

    refactor: consolidate mutable props w/ prisma dto

--- habitcircle/server/infra/db/prisma/dtos/post-prisma.dto.ts
+++ habitcircle/server/infra/db/prisma/dtos/post-prisma.dto.ts
@@ -1,12 +1,9 @@
-export type PostMutablePropsPrismaDto = {
-    photoKey: string | undefined;
-    caption: string | undefined; 
-}
-
 export type PostPrismaDto = {
     id: string;
     createdAt: Date;
     userId: string;
     habitId: string;
     completionId: string;
-} & PostMutablePropsPrismaDto
+    photoKey: string | undefined;
+    caption: string | undefined; 
+} 
