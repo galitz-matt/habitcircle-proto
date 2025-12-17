28256ac7e3c3ed50b765129f735c256eaa9d7b76
galitz-matt <galitz.matthew@gmail.com>
2025-12-16 20:10:56 -0500

    refactor: implement HashingService

--- habitcircle/server/infra/services/bcrypt-hashing.service.ts
+++ habitcircle/server/infra/services/bcrypt-hashing.service.ts
@@ -1,13 +1,14 @@
+import { HashingService } from "@/server/application/services/hashing.service";
 import bcrypt from "bcryptjs"
 
-export class BcryptHashingService {
+export class BcryptHashingService implements HashingService {
     constructor(readonly salt: number) {}
 
     async hash(s: string): Promise<string> {
         return await bcrypt.hash(s, this.salt);
     }
 
-    async compare(hash1: string, hash2: string): Promise<boolean> {
-        return await bcrypt.compare(hash1, hash2);
+    async compare(password: string, hash: string): Promise<boolean> {
+        return await bcrypt.compare(password, hash);
     }
 }
