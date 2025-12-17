460eecc28325129c163ce6f83028abf1f60d0d28
galitz-matt <galitz.matthew@gmail.com>
2025-12-16 22:07:46 -0500

    feat: define and implement session repo

---
+++ habitcircle/server/infra/redis/repositories/session.redis-repository.ts
@@ -0,0 +1,26 @@
+import { SessionRepository } from "@/server/application/repositories/session.repository";
+import { Session } from "@/server/application/models/session.model"
+import { redisClient } from "../redis.client";
+
+export class SessionRedisRepository implements SessionRepository {
+    async create(session: Session, ttlSeconds: number): Promise<void> {
+        await redisClient.set(
+            this.key(session.token),
+            JSON.stringify(session),
+            { EX: ttlSeconds }
+        );
+    }
+
+    async findByToken(sessionToken: string): Promise<Session | null> {
+        const raw = await redisClient.get(this.key(sessionToken));
+        return raw ? JSON.parse(raw) as Session : null;
+    }
+
+    async delete(sessionToken: string): Promise<void> {
+        await redisClient.del(this.key(sessionToken));
+    }
+
+    private key(token: string) {
+        return `session:${token}`
+    }
+}
