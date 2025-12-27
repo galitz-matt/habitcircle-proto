c1121b62a62c7014c753271bf2a3522edf004184
galitz-matt <galitz.matthew@gmail.com>
2025-12-27 15:18:09 -0500

    refactor: extract auth and profile props

--- habitcircle/server/domain/entities/user.entity.ts
+++ habitcircle/server/domain/entities/user.entity.ts
@@ -1,58 +1,46 @@
 import { IdGenerator } from "@/lib/utils";
-import { Username } from "@/server/domain/value-objects/username.value-object";
-import { Biography } from "@/server/domain/value-objects/biography.value-object";
-import { CredentialsAccount } from "./auth/credentials-account.entity";
-import { OAuthAccount } from "./auth/oauth-account.entity";
-import { OAuthAccountManager } from "../value-objects/auth/oauth-account-manager.value-object";
-import { DomainError } from "@/lib/errors";
+import { Username } from "../value-objects/user/username.value-object";
+import { Biography } from "../value-objects/user/biography.value-object";
+import { CredentialsAccount, CreateCredentialsAccountProps } from "./auth/credentials-account.entity";
+import { OAuthAccount, CreateOAuthAccountProps } from "./auth/oauth-account.entity";
+import { UserProfile, CreateUserProfileProps } from "../value-objects/user/user-profile.value-object";
+import { UserAuthentication } from "./auth/user-authentication.entity";
 
-// TODO: 1238use objects instead of VOs here
-type CreateUserOptions = {
-    biography?: string;
-    profilePictureKey?: string;
-    credentialsAccount?: CredentialsAccount;
-    oauthAccounts?: OAuthAccount[];
+type AuthenticationMethod =
+    | { type: "credentials"; account: CreateCredentialsAccountProps }
+    | { type: "oauth"; accounts: CreateOAuthAccountProps[] }
+
+export type CreateUserProps = {
+    profile: CreateUserProfileProps 
+    auth: AuthenticationMethod
 }
 
 export class User {
     private constructor(
         private readonly _id: string,
         private _createdAt: Date,
-        private _username: Username,
-        private _oauthAccountManager: OAuthAccountManager,
-        private _biography?: Biography,
-        private _profilePictureKey?: string,
-        private _credentialsAccount?: CredentialsAccount,
+        private profile: UserProfile,
+        private auth: UserAuthentication
     ) {}
 
-    static create(
-        username: string,
-        options: CreateUserOptions
-    ) {
-        if (this.isMixingAuthentication(options.oauthAccounts, options.credentialsAccount)) {
-            throw new DomainError("Cannot mix credentials and oauth authentication for one user");
-        }
-        const usernameVO = Username.create(username);
-        const oauthAccountManager = OAuthAccountManager.create(options.oauthAccounts ?? [])
-        const biographyVO = options.biography ? Biography.create(options.biography) : undefined;
-
+    static create(props: CreateUserProps) {
+        const profile = UserProfile.create(props.profile)
+        const auth = props.auth.type === "credentials"
+            ? UserAuthentication.withCredentials(props.auth.account)
+            : UserAuthentication.withOAuth(props.auth.accounts);
         return new User(
             IdGenerator.new(),
             new Date(),
-            usernameVO,
-            oauthAccountManager,
-            biographyVO,
-            options.profilePictureKey,
-            options.credentialsAccount,
-        );
+            profile, 
+            auth);
     };
 
     equals(other: User): boolean {
         return !!other && this._id === other.id;
     }
 
-    addOAuthAccount(oauthAccount: OAuthAccount): this {
-        this._oauthAccountManager = this._oauthAccountManager.addAccount(oauthAccount);
+    addOAuthAccount(props: CreateOAuthAccountProps): this {
+        this.auth.addOAuthAccount(props);
         return this;
     }
     
@@ -65,47 +53,37 @@ export class User {
     }
 
     get username(): Username {
-        return this._username;
+        return this.profile.username;
     }
 
     get biography(): Biography | undefined {
-        return this._biography;
+        return this.profile.biography;
     }
 
     get profilePictureKey(): string | undefined {
-        return this._profilePictureKey;
+        return this.profile.profilePictureKey;
     }
 
     get credentialsAccount(): CredentialsAccount | undefined {
-        return this._credentialsAccount;
+        return this.auth.credentialsAccount;
     }
 
     get oauthAccounts(): OAuthAccount[] {
-        return this._oauthAccountManager.accounts;
+        return this.auth.oauthAccounts;
     }
 
     static rehydrate(
         id: string,
         createdAt: Date,
-        username: Username,
-        oauthAccountManager: OAuthAccountManager,
-        biography?: Biography,
-        profilePictureKey?: string,
-        credentialsAccount?: CredentialsAccount,
+        profile: UserProfile,
+        auth: UserAuthentication
     ): User {
         /* Used exclusively by repositories to reconstitue from persistence */
         return new User(
             id,
             createdAt,
-            username,
-            oauthAccountManager,
-            biography,
-            profilePictureKey,
-            credentialsAccount
+            profile,
+            auth
         );
-    }
-
-    private static isMixingAuthentication(oauthAccounts?: OAuthAccount[], credentialsAccount?: CredentialsAccount): boolean {
-        return !!oauthAccounts && oauthAccounts.length > 0 && !!credentialsAccount;
     }
 }
