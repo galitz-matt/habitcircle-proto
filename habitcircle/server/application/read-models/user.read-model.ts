ff6edf887f81eeb0b5e112e70842384fd831f96c
galitz-matt <galitz.matthew@gmail.com>
2025-12-27 13:09:07 -0500

    refactor: pass primitives to method

--- habitcircle/server/application/read-models/user.read-model.ts
+++ habitcircle/server/application/read-models/user.read-model.ts
@@ -1,7 +1,6 @@
-import type { OAuthIdentity } from "@/server/domain/value-objects/auth/oauth-identity.value-object";
 import type { User } from "@/server/domain/entities/user.entity";
 
 export interface UserReadModel {
     findUsersByOAuthEmailAddress(emailAddress: string): Promise<User[]>;
-    findUserByOAuthIdentity(oauthIdentity: OAuthIdentity): Promise<User | null>;
+    findUserByOAuthIdentity(provider: string, providerAccountId: string): Promise<User | null>;
 }
--- habitcircle/server/infra/prisma/read-models/user.prisma-read-model.ts
+++ habitcircle/server/infra/prisma/read-models/user.prisma-read-model.ts
@@ -2,7 +2,6 @@ import { UserReadModel } from "@/server/application/read-models/user.read-model"
 import { PrismaClient } from "../generated";
 import { User } from "@/server/domain/entities/user.entity";
 import { UserPrismaMapper } from "../mappers/user.prisma-mapper";
-import { OAuthIdentity } from "@/server/domain/value-objects/auth/oauth-identity.value-object";
 
 export class UserPrismaReadModel implements UserReadModel {
     constructor(private readonly prisma: PrismaClient) {}
@@ -23,12 +22,12 @@ export class UserPrismaReadModel implements UserReadModel {
         return users.map(u => UserPrismaMapper.toDomain(u));
     }
 
-    async findUserByOAuthIdentity(oauthIdentity: OAuthIdentity): Promise<User | null> {
+    async findUserByOAuthIdentity(provider: string, providerAccountId: string): Promise<User | null> {
         const account = await this.prisma.oAuthAccount.findUnique({
             where: { 
                 provider_providerAccountId: {
-                    provider: oauthIdentity.provider,
-                    providerAccountId: oauthIdentity.providerAccountId
+                    provider,
+                    providerAccountId
                 }
             },
             select: {
