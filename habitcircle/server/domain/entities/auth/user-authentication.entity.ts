00d136ba6aad13222170f66955d496ab14751409
galitz-matt <galitz.matthew@gmail.com>
2025-12-27 15:17:14 -0500

    feat: add oauth account logic, rehydration method

--- habitcircle/server/domain/entities/auth/user-authentication.entity.ts
+++ habitcircle/server/domain/entities/auth/user-authentication.entity.ts
@@ -1,11 +1,12 @@
-import { OAuthAccountManager } from "./oauth-account-manager.entity";
+import { DomainError } from "@/lib/errors";
+import { OAuthAccountManager } from "../../value-objects/auth/oauth-account-manager.value-object";
 import { CredentialsAccount, CreateCredentialsAccountProps } from "./credentials-account.entity";
 import { OAuthAccount, CreateOAuthAccountProps } from "./oauth-account.entity";
 
 export class UserAuthentication {
     private constructor(
-        private credentialsAccount?: CredentialsAccount,
-        private oauthAccountManager?: OAuthAccountManager
+        private _credentialsAccount?: CredentialsAccount,
+        private _oauthAccountManager?: OAuthAccountManager
     ) {}
 
     static withCredentials(props: CreateCredentialsAccountProps) {
@@ -21,5 +22,42 @@ export class UserAuthentication {
             undefined,
             OAuthAccountManager.create(oauthAccounts)
         );
+    }
+
+    addOAuthAccount(props: CreateOAuthAccountProps) {
+        if (this.usesCredentials()) {
+            throw new DomainError("User uses credentials for auth, cannot mix auth methods");
+        }
+        if (!this._oauthAccountManager) {
+            this._oauthAccountManager = OAuthAccountManager.create([]);
+        }
+        const account = OAuthAccount.create(props);
+        this._oauthAccountManager = this._oauthAccountManager.addAccount(account);
+    }
+
+    get credentialsAccount(): CredentialsAccount | undefined {
+        return this.credentialsAccount;
+    }
+
+    get oauthAccounts(): OAuthAccount[] {
+        return this._oauthAccountManager?.accounts ?? [];
+    }
+
+    usesCredentials(): boolean {
+        return !!this._credentialsAccount;
+    }
+
+    usesOAuth(): boolean {
+        return !!this._oauthAccountManager;
+    }
+
+    static rehydrate(
+        credentialsAccount?: CredentialsAccount,
+        oauthAccountManager?: OAuthAccountManager
+    ): UserAuthentication {
+        return new UserAuthentication(
+            credentialsAccount,
+            oauthAccountManager
+        )
     }
 }
