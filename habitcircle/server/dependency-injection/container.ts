b394845d6837e56d26c958bd1c16b15377d4928f
galitz-matt <galitz.matthew@gmail.com>
2026-01-01 17:58:01 -0500

    feat: register services, organize

--- habitcircle/server/dependency-injection/container.ts
+++ habitcircle/server/dependency-injection/container.ts
@@ -4,22 +4,37 @@ import { UserRepository } from "@/server/application/repositories/user.repositor
 import { CirclePrismaRepository } from "@/server/infra/prisma/repositories/circle.prisma-repository";
 import { UserPrismaRepository } from "@/server/infra/prisma/repositories/user.prisma-repository";
 import { PrismaClient } from "@/prisma/generated";
-import type { AuthenticationReadModel } from "../application/read-models/authentication.read-model";
+import { AuthenticationReadModel } from "../application/read-models/authentication.read-model";
 import { AuthenticationPrismaReadModel } from "../infra/prisma/read-models/authentication.prisma-read-model";
-import type { HashingService } from "@/server/application/services/hashing.service";
+import { HashingService } from "@/server/application/services/hashing.service";
 import { BcryptHashingService } from "@/server/infra/services/bcrypt-hashing.service";
 import { SessionRepository } from "../application/repositories/session.repository";
 import { SessionRedisRepository } from "../infra/redis/repositories/session.redis-repository";
+import { AuthenticationService } from "../application/services/authentication.service";
+import { UserReadModel } from "../application/read-models/user.read-model";
+import { UserPrismaReadModel } from "../infra/prisma/read-models/user.prisma-read-model";
+import { LinkSessionRepository } from "../application/repositories/link-session.repository";
+import { LinkSessionRedisRepository } from "../infra/redis/repositories/link-session.redis-repository";
 
 const saltRounds = Number(process.env.SALT_ROUNDS);
 
-container.register<AuthenticationReadModel>("AuthenticationReadModel", { useClass: AuthenticationPrismaReadModel })
-
-container.register<CircleRepository>("CircleRepository", { useClass: CirclePrismaRepository });
-container.register<UserRepository>("UserRepository", { useClass: UserPrismaRepository });
-container.register<SessionRepository>("SessionRepository", { useClass: SessionRedisRepository });
+// SERVICES
 container.register<HashingService>("HashingService", { useValue: new BcryptHashingService(saltRounds) });
 
+// READ MODELS
+container.register<AuthenticationReadModel>("AuthenticationReadModel", { useClass: AuthenticationPrismaReadModel })
+container.register<UserReadModel>("UserReadModel", { useClass: UserPrismaReadModel })
+
+// WRITE MODELS
+
+
+// REPOSITORIES
+container.register<UserRepository>("UserRepository", { useClass: UserPrismaRepository });
+container.register<LinkSessionRepository>("LinkSessionRepository", { useClass: LinkSessionRedisRepository });
+container.register<SessionRepository>("SessionRepository", { useClass: SessionRedisRepository });
+
+
+// EXTERNALS
 container.registerInstance(PrismaClient, new PrismaClient());
 
 export { container };
