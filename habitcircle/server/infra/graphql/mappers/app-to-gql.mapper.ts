a392c8c748f598491668d515cab1cbcb726f3d77
galitz-matt <galitz.matthew@gmail.com>
2026-01-05 22:35:14 -0500

    feat: app to gql login result mapper

---
+++ habitcircle/server/infra/graphql/mappers/app-to-gql.mapper.ts
@@ -0,0 +1,26 @@
+import type { LoginResult } from "@/server/application/dtos/results/login.result";
+import { GqlLoginResult } from "../types/login-result.graphql";
+
+export class AppToGqlMapper {
+    static toGqlLoginResult(result: LoginResult): GqlLoginResult | null {
+        switch (result.type) {
+            case "SUCCESS":
+                return {
+                    __typename: "LoginSuccess",
+                    userId: result.session.userId
+                };
+            case "INVALID_CREDENTIALS":
+                return {
+                    __typename: "InvalidCredentials",
+                    reason: "Invalid username or password",
+                };
+            case "PENDING_LINK":
+                return {
+                    __typename: "PendingLink",
+                    linkSession: result.linkSession
+                };
+            default:
+                throw new Error(`Unhandle LoginResult: ${result.type}`);
+        }
+    }
+}
