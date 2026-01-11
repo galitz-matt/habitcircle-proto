5320908b93494f23309ca73d5dab00c9beaa7112
galitz-matt <galitz.matthew@gmail.com>
2026-01-11 14:47:43 -0500

    feat: define mapping for GqlMeResult

--- habitcircle/server/infra/graphql/mappers/app-to-gql.mapper.ts
+++ habitcircle/server/infra/graphql/mappers/app-to-gql.mapper.ts
@@ -1,5 +1,7 @@
 import type { LoginResult } from "@/server/application/dtos/results/login.result";
 import { GqlLoginResult } from "../types/auth/login-result.graphql";
+import { ResolveSessionResult } from "@/server/application/dtos/results/resolve-session.result";
+import { GqlMeResult } from "../types/auth/me-result.graphql";
 
 export class AppToGqlMapper {
     static toGqlLoginResult(result: LoginResult): GqlLoginResult {
@@ -21,6 +23,21 @@ export class AppToGqlMapper {
                 };
             default:
                 throw new Error(`Unhandle LoginResult: ${result.type}`);
+        }
+    }
+
+    static toGqlMeResult(result: ResolveSessionResult): GqlMeResult {
+        switch (result.type) {
+            case "SUCCESS":
+                return {
+                    __typename: "Authenticated",
+                    userId: result.userId
+                }
+            case "INVALID_TOKEN":
+                return {
+                    __typename: "Unauthenticated",
+                    reason: "Invalid session token"
+                }
         }
     }
 }
