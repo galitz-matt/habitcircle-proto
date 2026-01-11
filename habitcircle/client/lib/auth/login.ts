cc0e19198354a5fcfce5dd90cacfe2d240fcb761
galitz-matt <galitz.matthew@gmail.com>
2026-01-11 12:46:08 -0500

    feat: client entry point for loginWithCredentials call

---
+++ habitcircle/client/lib/auth/login.ts
@@ -0,0 +1,18 @@
+import { LoginResult, LoginWithCredentialsDocument, LoginWithCredentialsMutation, LoginWithCredentialsMutationVariables } from "@/generated/graphql";
+import { graphqlFetch } from "../graphql/client";
+
+export async function loginWithCredentials(
+    username: string,
+    password: string
+): Promise<LoginResult> {
+    const res = await graphqlFetch<
+        LoginWithCredentialsMutation,
+        LoginWithCredentialsMutationVariables
+    >(LoginWithCredentialsDocument, {
+        input: {
+            username,
+            password
+        },
+    });
+    return res.loginWithCredentials;
+}
